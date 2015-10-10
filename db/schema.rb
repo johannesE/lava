b449490a847fbe5a0d913d2ba673ba50036f370f
johannesE <johannes.eifert@unifr.ch>
2015-10-10 17:23:28 +0200

    migration for pg

--- db/schema.rb
+++ db/schema.rb
@@ -40,11 +40,13 @@ ActiveRecord::Schema.define(version: 20151010110403) do
   create_table "posts", force: :cascade do |t|
     t.string   "title"
     t.text     "body"
+    t.integer  "user_id"
     t.integer  "author_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
   add_index "posts", ["author_id"], name: "index_posts_on_author_id"
+  add_index "posts", ["user_id"], name: "index_posts_on_user_id"
 
 end
